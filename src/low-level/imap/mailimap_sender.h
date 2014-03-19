ed7102f46c68b796c0d40e2a9f3ba6eea791f67a
Dunemaster <vklava@yandex.ru>
2014-03-19 22:10:44 +0400

    fix line

--- src/low-level/imap/mailimap_sender.h
+++ src/low-level/imap/mailimap_sender.h
@@ -118,6 +118,7 @@ int
 mailimap_uid_search_send(mailstream * fd, const char * charset,
 			 struct mailimap_search_key * key);
 
+
 int mailimap_search_key_send(mailstream * fd,
                              struct mailimap_search_key * key);
   
