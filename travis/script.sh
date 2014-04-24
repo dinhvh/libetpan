c2b2d0fe79d9fc47eca291b62ef9ec1c7ea6696e
Hoa V. Dinh <dinh.viet.hoa@gmail.com>
2014-04-24 15:24:04 -0700

    Call configure before make

--- travis/script.sh
+++ travis/script.sh
@@ -6,6 +6,7 @@ distdir=libetpan-travis-build
 make dist distdir=$distdir
 tar xzf $distdir.tar.gz
 cd $distdir
+./configure --with-curl=no --disable-db --with-expat=no
 make
 cd tests
 make imap-sample
