4e858d4cdaa8073b5d6ccf2099070da8519d6dac
CodaFi <devteam.codafi@gmail.com>
2014-08-13 14:54:44 -0600

    Move members

--- src/low-level/nntp/newsnntp_types.h
+++ src/low-level/nntp/newsnntp_types.h
@@ -92,11 +92,12 @@ struct newsnntp
   char * nntp_response;
 
   time_t nntp_timeout;
-  mailprogress_function * nntp_progress_fun;
-  void * nntp_progress_context;
   
   void (* nntp_logger)(newsnntp * session, int log_type, const char * str, size_t size, void * context);
   void * nntp_logger_context;
+  
+  mailprogress_function * nntp_progress_fun;
+  void * nntp_progress_context;
 };
 
 struct newsnntp_group_info
