4113c3ae25abc3f0be21f5210cbf181d8673fe7f
Hoa V. Dinh <dinh.viet.hoa@gmail.com>
2014-10-06 14:54:38 -0700

    Fixed build

--- travis/script.sh
+++ travis/script.sh
@@ -13,9 +13,9 @@ if test "x`uname`" = xLinux ; then
   make imap-sample
 else
   echo Building library for iPhoneOS
-  xctool -project build-mac/libetpan.xcodeproj -sdk iphoneos7.1 -scheme "libetpan ios" build ARCHS="armv7 armv7s arm64"
+  xctool -project build-mac/libetpan.xcodeproj -sdk iphoneos8.0 -scheme "libetpan ios" build ARCHS="armv7 armv7s arm64"
   echo Building library for iPhoneSimulator
-  xctool -project build-mac/libetpan.xcodeproj -sdk iphonesimulator7.1 -scheme "libetpan ios" build ARCHS="i386 x86_64"
+  xctool -project build-mac/libetpan.xcodeproj -sdk iphonesimulator8.0 -scheme "libetpan ios" build ARCHS="i386 x86_64"
 
   echo Building library for Mac
   xctool -project build-mac/libetpan.xcodeproj -sdk macosx10.8 -scheme "static libetpan" build
