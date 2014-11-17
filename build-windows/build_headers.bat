88bebbfc834342ba4259ee8a0cdc784c3bb2103e
Hoa V. Dinh <dinh.viet.hoa@gmail.com>
2014-11-17 15:56:15 -0800

    fixed build_headers.bat behavior

--- build-windows/build_headers.bat
+++ build-windows/build_headers.bat
@@ -1,5 +1,4 @@
 @mkdir include
 @mkdir include\libetpan
-@for /R ..\src\ %%x in (*.h) do @copy "%%x" include\libetpan
-@for %%x in (*.h) do @copy "%%x" include\libetpan
+@for /F "delims=" %%a in (build_headers.list) do @copy "%%a" include\libetpan
 @echo "done" >_headers_depends
