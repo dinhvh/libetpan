13a73bfa2f20cfd123338603edde537eb7a5d844
Volker Birk <vb@dingens.org>
2015-03-08 23:27:23 +0100

    adding quoted-printable support to data-types

---
+++ src/data-types/quoted_printable.h
@@ -0,0 +1,60 @@
+/*
+ * libEtPan! -- a mail stuff library
+ *
+ * Copyright (C) 2015 - Hoa V. DINH, Volker Birk
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the libEtPan! project nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * $Id: quoted_printable.h,v 1.0 2015/03/08 20:56:00 fdik Exp $
+ */
+
+#ifndef __QUOTED_PRINTABLE_H__
+#define __QUOTED_PRINTABLE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef LIBETPAN_CONFIG_H
+#   include "libetpan-config.h"
+#endif
+
+LIBETPAN_EXPORT
+char * etpan_make_full_quoted_printable(const char * display_charset,
+    const char * phrase);
+
+LIBETPAN_EXPORT
+char * etpan_make_quoted_printable(const char * display_charset, const
+    char * phrase, int subject);
+
+#ifdef __cplusplus
+}
+#endif    
+
+#endif // __QUOTED_PRINTABLE_H__
+
