65486f2fe26625a557eddcb9e9fdf5ae4674db9a
Lin Zhan <zhanleewo@gmail.com>
2015-03-26 16:44:15 +0800

    Add NTLM Support!
    NTLM Response is plain text, so we do not need to decoded it.
    
    ##The workflow about NTLM:
    After client sent "AUTH NTLM" to server, The Exchange server 2010 will respond non-base64 encoded string "443 NTLM supported.", Actually The Exchange server 2003 just respond "443" without body.
    
    Client-------------------------->Server
    AUTH NTLM----------------->
    <---------------------------------443 NTLM Supported (Non-base64 encoded string)
    NTLMSSP....----------------->
    <---------------------------------443 NTLMSSP........
    BOB_MESSAGE------------>
    <---------------------------------235 Authentication successfully.

--- src/low-level/smtp/mailsmtp.c
+++ src/low-level/smtp/mailsmtp.c
@@ -939,6 +939,10 @@ int mailsmtp_auth_type(mailsmtp * session,
   case MAILSMTP_AUTH_DIGEST_MD5:
     return mailesmtp_auth_sasl(session, "DIGEST-MD5",
         hostname, NULL, NULL, user, user, pass, NULL);
+        
+  case MAILSMTP_AUTH_NTLM:
+    return mailesmtp_auth_sasl(session, "NTLM",
+        hostname, NULL, NULL, user, user, pass, NULL);
     
   default:
     return MAILSMTP_ERROR_NOT_IMPLEMENTED;
@@ -1347,26 +1351,30 @@ int mailesmtp_auth_sasl(mailsmtp * session, const char * auth_type,
         }
         
         response_len = (unsigned int) strlen(session->response);
-        max_decoded = response_len * 3 / 4;
-        decoded = malloc(max_decoded + 1);
-        if (decoded == NULL) {
-          res = MAILSMTP_ERROR_MEMORY;
-          goto free_sasl_conn;
-        }
+        if(strncasecmp(auth_type, "NTLM", strlen(auth_type)) == 0) {
+          r = sasl_client_step(session->smtp_sasl.sasl_conn, session->response, (unsigned) response_len, NULL, &sasl_out, &sasl_out_len);
+        } else {
+          max_decoded = response_len * 3 / 4;
+          decoded = malloc(max_decoded + 1);
+          if (decoded == NULL) {
+            res = MAILSMTP_ERROR_MEMORY;
+            goto free_sasl_conn;
+          }
         
-        r = sasl_decode64(session->response, response_len,
+          r = sasl_decode64(session->response, response_len,
             decoded, max_decoded + 1, &decoded_len);
         
-        if (r != SASL_OK) {
-          free(decoded);
-          res = MAILSMTP_ERROR_MEMORY;
-          goto free_sasl_conn;
-        }
+          if (r != SASL_OK) {
+            free(decoded);
+            res = MAILSMTP_ERROR_MEMORY;
+            goto free_sasl_conn;
+          }
         
-        r = sasl_client_step(session->smtp_sasl.sasl_conn,
+          r = sasl_client_step(session->smtp_sasl.sasl_conn,
             decoded, decoded_len, NULL, &sasl_out, &sasl_out_len);
         
-        free(decoded);
+          free(decoded);
+        }
         
         if ((r != SASL_CONTINUE) && (r != SASL_OK)) {
           res = MAILSMTP_ERROR_AUTH_LOGIN;
