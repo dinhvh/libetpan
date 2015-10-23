2cd6d21f73aec00c1f05b54202709b21ae921be3
Hiroki Mori <yamori813@yahoo.co.jp>
2015-10-23 10:44:44 +0900

    add sdk version condition

--- src/data-types/mailstream_cfstream.c
+++ src/data-types/mailstream_cfstream.c
@@ -34,11 +34,13 @@
 #if HAVE_CFNETWORK
 #include <CoreFoundation/CoreFoundation.h>
 #include <TargetConditionals.h>
-#include <Security/SecTrust.h>
 #if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
 #include <CFNetwork/CFNetwork.h>
 #include <Security/Security.h>
 #else
+#if __MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_6
+#include <Security/Security.h>
+#endif
 #include <CoreServices/CoreServices.h>
 #endif
 #endif
