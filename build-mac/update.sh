151b63de004888f954fa7942ebba849924b3c2a3
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2016-03-26 23:03:49 -0700

    Fixed config build for Mac/iOS

--- build-mac/update.sh
+++ build-mac/update.sh
@@ -43,7 +43,7 @@ if test "x$ACTION" = xbuild -o "x$ACTION" = xinstall ; then
     tar xzf build-mac/autogen-result.tar.gz
     export SDKROOT=
     export IPHONEOS_DEPLOYMENT_TARGET=
-    ./configure --enable-debug > "$logfile" 2>&1
+    ./configure --with-expat=no --with-curl=no --enable-debug > "$logfile" 2>&1
     if [[ "$?" != "0" ]]; then
       cat "$logfile"
       echo "configure failed"
