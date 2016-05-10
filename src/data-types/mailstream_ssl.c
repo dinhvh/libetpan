c9aa58d7b009d48bf5f690b4aa62d5672fe76d29
DarovskikhAndrei <Darovskikh.Andrei@gmail.com>
2016-05-10 09:31:03 +0300

    BuildFix

--- src/data-types/mailstream_ssl.c
+++ src/data-types/mailstream_ssl.c
@@ -67,12 +67,13 @@
 #else
 #	include <sys/time.h>
 #	include <sys/types.h>
+#   if USE_POLL
 #       ifdef HAVE_SYS_POLL_H
 #	        include <sys/poll.h>
 #       endif
 #   else 
 #       ifdef HAVE_SELECT_H
-#	        include <sys/poll.h>
+#	        include <sys/select.h>
 #       endif
 #   endif
 #endif
