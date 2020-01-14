aca0d88318685dc307a7e414cac9b9cdcfb63b7b
alex2grad <alex2grad@gmail.com>
2020-01-14 16:27:39 -0500

    mailimap_sort: return empty list if nothing found
    If IMAP server found nothing, mailimap_sort and mailimap_uid_sort have to return empty list instead of ERROR.

--- src/low-level/imap/mailimap_sort.c
+++ src/low-level/imap/mailimap_sort.c
@@ -135,7 +135,7 @@ mailimap_sort(mailimap * session, const char * charset,
   session->imap_response_info->rsp_extension_list = NULL;
   
   if (sort_result == NULL) {
-    return MAILIMAP_ERROR_EXTENSION;
+    sort_result = clist_new();
   }
   
   error_code = response->rsp_resp_done->rsp_data.rsp_tagged->rsp_cond_state->rsp_type;
@@ -213,7 +213,7 @@ mailimap_uid_sort(mailimap * session, const char * charset,
   session->imap_response_info->rsp_extension_list = NULL;
   
   if (sort_result == NULL) {
-    return MAILIMAP_ERROR_EXTENSION;
+    sort_result = clist_new();
   }
   
   error_code = response->rsp_resp_done->rsp_data.rsp_tagged->rsp_cond_state->rsp_type;
