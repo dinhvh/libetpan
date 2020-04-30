1633d382d32bd6534c5c774a893add164866d36d
Ranier Vilela <ranier_gyn@hotmail.com>
2020-04-29 22:04:39 -0300

    Fix potential uninitialized var type used
    Fix style to remove warning.

--- src/low-level/imap/enable.c
+++ src/low-level/imap/enable.c
@@ -289,14 +289,11 @@ mailimap_enable_extension_parse(int calling_parser, mailstream * fd,
     case MAILIMAP_EXTENDED_PARSER_RESPONSE_DATA:
 			r = mailimap_enable_parse(fd, buffer, parser_ctx, &cur_token,
 		    &capabilities, progr_rate, progr_fun);
-      if (r == MAILIMAP_NO_ERROR) {
-        type = MAILIMAP_ENABLE_TYPE_ENABLE;
-        data = capabilities;
-      }
-      
-      if (r != MAILIMAP_NO_ERROR) {
+      if (r != MAILIMAP_NO_ERROR)
         return r;
-      }
+
+      type = MAILIMAP_ENABLE_TYPE_ENABLE;
+      data = capabilities;
       
       ext_data = mailimap_extension_data_new(&mailimap_extension_enable,
                                              type, data);
